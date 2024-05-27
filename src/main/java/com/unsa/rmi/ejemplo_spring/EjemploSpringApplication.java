e3c0f65f34dfdd0bd9689636882baf0ceeb97025
Gabriel <gmachicaoq@unsa.edu.pe>
2024-05-26 21:16:55 -0500

    Configurando el proyecto como cliente

--- src/main/java/com/unsa/rmi/ejemplo_spring/EjemploSpringApplication.java
+++ src/main/java/com/unsa/rmi/ejemplo_spring/EjemploSpringApplication.java
@@ -1,13 +1,31 @@
 package com.unsa.rmi.ejemplo_spring;
 
+import com.unsa.rmi.ejemplo_spring.service.CalculatorService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.remoting.rmi.RmiProxyFactoryBean;
+
+import java.rmi.RemoteException;
 
 @SpringBootApplication
 public class EjemploSpringApplication {
+	@Bean
+	RmiProxyFactoryBean rmiProxy() {
+		RmiProxyFactoryBean bean = new RmiProxyFactoryBean();
+		bean.setServiceInterface(CalculatorService.class);
+		bean.setServiceUrl("rmi://localhost:1099/CalculatorServices");
+		return bean;
+	}
+	public static void main(String[] args) throws RemoteException {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EjemploSpringApplication.class, args);
+		CalculatorService calculatorService = SpringApplication.run(EjemploSpringApplication.class, args)
+				.getBean(CalculatorService.class);
+
+		int resultAdd = calculatorService.add(5, 3);
+		int resultSubtract = calculatorService.subtract(5, 3);
+		System.out.println("Addition result: " + resultAdd);
+		System.out.println("Subtraction result: " + resultSubtract);
 	}
 
 }
