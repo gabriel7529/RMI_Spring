db6cde3c5ea1324a1e148adb9774a469e092ee08
Gabriel <gmachicaoq@unsa.edu.pe>
2024-05-26 19:51:19 -0500

    Iniciando el proyecto

---
+++ src/test/java/com/unsa/rmi/ejemplo_spring/EjemploSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unsa.rmi.ejemplo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
